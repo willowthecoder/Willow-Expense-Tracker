311a7634358b19460ed425b2b965f9e7c1ec346e
Willow <thelivingviking777@gmail.com>
2025-07-14 13:29:16 +0530

    v1 completed

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 // apollo
 import { ApolloServer } from "@apollo/server";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import { expressMiddleware } from "@as-integrations/express5";
+import { expressMiddleware } from "@as-integrations/express4";
 import passport from "passport";
 import session from "express-session";
 import connectMongo from "connect-mongodb-session";
 import { buildContext } from "graphql-passport";
 import { configurePassport } from "./passport/passport.config.js";
+import path from "path";
 
 // express
 import express from "express";
@@ -20,6 +21,7 @@ import mergetypeDef from "./typeDefs/index.js";
 configurePassport();
 
 dotenv.config();
+const __dirname = path.resolve();
 const app = express();
 const httpServer = http.createServer(app);
 const MongoDBStore = connectMongo(session);
@@ -68,6 +70,12 @@ app.use(
     context: async ({ req, res }) => buildContext({ req, res }),
   })
 );
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+});
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 await connectDB();
